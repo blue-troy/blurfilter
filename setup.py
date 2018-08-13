d915c4497252fac8099e491b0637b7eb23ab4a6b
heyixin <764118015@qq.com>
2018-08-13 20:43:50 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='filter',
+      version='1.0',
+      description='filter by image blur',
+      author='blue troy',
+      author_email='764118015@qq.com',
+      packages=['filter'], )
