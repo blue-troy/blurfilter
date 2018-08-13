ffa313f1255701b20c0c215ef4db8231796567b9
heyixin <764118015@qq.com>
2018-08-13 21:48:05 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='blur filter',  # 应用名
+    name='blurfilter',  # 应用名
     version='1.0',  # 版本号
     packages=['blurfilter'],
     install_requires=[  # 依赖列表
