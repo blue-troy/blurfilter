3c6bb76c4b86afaf805d49820568b6d276f5fee9
heyixin <764118015@qq.com>
2018-08-13 22:12:06 +0800

    add setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='blurfilter',  # 应用名
     version='1.0',  # 版本号
-    packages=['blurfilter'],
+    packages=find_packages(),
     install_requires=[  # 依赖列表
         'opencv-python'
     ]
